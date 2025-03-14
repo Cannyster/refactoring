52998638a56d4b7b96526667c89381677220014a
Jhonanthan <jhonanthanwork@gmail.com>
2025-03-14 12:44:18 -0300

    Base inicial do codigo, com primeira refatoração

---
+++ code.js
@@ -0,0 +1,63 @@
+import fs from "fs";
+
+const plays = JSON.parse(fs.readFileSync("./plays.json", "utf-8"));
+const invoice = JSON.parse(fs.readFileSync("./invoice.json", "utf-8"));
+
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  //Acessando o primeiro elemento para que ele seja iterável
+  const firstInvoice = invoice[0];
+
+  const format = new Intl.NumberFormat("en-us", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of firstInvoice.performances) {
+    const play = plays[perf.playId];
+    let thisAmount = amountFor(perf, play);
+
+    // soma créditos por volume
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    // soma um crédito extra para cada dez espectadores de comédia
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // exibe a linha para esta requisição
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    } seats)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `Amount owed id ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
+
+function amountFor(aPerformance, play) {
+  let result = 0;
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`unknow type: ${play.type}`);
+  }
+  return result;
+}
+
+console.log(statement(invoice, plays));
